8dbde850a6733dc3c74ba0c952a946a210c8226c
Danelle2403 <danelle.goh.2024@computing.smu.edu.sg>
2026-03-19 16:11:10 +0800

    Update api.js

--- ui/js/api.js
+++ ui/js/api.js
@@ -1,1 +1,15 @@
+const API_BASE = "http://localhost:8000"; // API Gateway URL
 
+async function getEvents() {
+    const res = await fetch(`${API_BASE}/events`);
+    return res.json();
+}
+
+async function buyTicket(data) {
+    const res = await fetch(`${API_BASE}/purchase/checkout`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(data)
+    });
+    return res.json();
+}
